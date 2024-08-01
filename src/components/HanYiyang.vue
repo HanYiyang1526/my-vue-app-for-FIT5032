98ee39176eb43792f5e364a321626ae13e045209
HanYiyang1526 <hanyiyang15206@gmail.com>
2024-08-02 07:31:53 +1000

    first push

---
+++ src/components/HanYiyang.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>MyName</h1>
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Han Yiyang' //
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
